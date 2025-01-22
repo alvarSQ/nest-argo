afc8da1d5f3009102f8ad28923bc2d8e40a38787
Yaroslav Kh <yarik77@list.ru>
2025-01-22 09:46:06 +0500

    teg, users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from '@/ormconfig';
+import { TagModule } from '@/tag/tag.module';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [TagModule, TypeOrmModule.forRoot(ormconfig), UserModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/migrations/1737454363804-schema-update.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class SchemaUpdate1737454363804 implements MigrationInterface {
+    name = 'SchemaUpdate1737454363804'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "tags" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_e7dc17249a1148a1970748eda99" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "tags"`);
+    }
+
+}
